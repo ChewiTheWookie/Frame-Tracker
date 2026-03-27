13549e76981bee0d5b520eae62028c4ed9be12e3
Colin L <darkblade.ca@gmail.com>
2026-03-27 13:03:24 -0500

    refactor: :pencil2: move time store useEffect from App.tsx to AppInitializer.tsx

--- src/components/AppInitializer/AppInitializer.tsx
+++ src/components/AppInitializer/AppInitializer.tsx
@@ -2,26 +2,44 @@ import { useEffect } from "react";
 import { listen } from "@tauri-apps/api/event";
 import { useMasteryStore } from "../../stores/useMasteryStore";
 import { useTaskStore } from "../../stores/useTaskStore";
+import { useTimeStore } from "../../stores/useTimeStore";
 
 export const AppInitializer = () => {
+    const updateTime = useTimeStore((state) => state.updateTime);
+
+    useEffect(() => {
+        const intervalId = setInterval(updateTime, 1000);
+        return () => clearInterval(intervalId);
+    }, [updateTime]);
+
     useEffect(() => {
         let unlistenSync: (() => void) | null = null;
         let unlistenReset: (() => void) | null = null;
+        let isMounted = true;
 
         const setupListeners = async () => {
-            unlistenSync = await listen("db-initial-sync-complete", () => {
+            const syncSub = await listen("db-initial-sync-complete", () => {
                 const state = useMasteryStore.getState();
                 state.fetchItems(state.itemIds.length > 0);
             });
 
-            unlistenReset = await listen("tasks-reset", () => {
+            const resetSub = await listen("tasks-reset", () => {
                 useTaskStore.getState().fetchTasks(true);
             });
+
+            if (isMounted) {
+                unlistenSync = syncSub;
+                unlistenReset = resetSub;
+            } else {
+                syncSub();
+                resetSub();
+            }
         };
 
         setupListeners();
 
         return () => {
+            isMounted = false;
             if (unlistenSync) unlistenSync();
             if (unlistenReset) unlistenReset();
         };
